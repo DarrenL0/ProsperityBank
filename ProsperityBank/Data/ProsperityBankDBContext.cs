ee719fec87ce0273b226fac27a166ec1ec724ef0
Darren <darren.lieu2000@gmail.com>
2022-07-18 21:55:11 +1000

    Implemented connection to database

---
+++ ProsperityBank/Data/ProsperityBankDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProsperityBank.Models;
+
+namespace Rental.Data
+{
+    public class ProsperityBankDBContext :DbContext
+    {
+        public ProsperityBankDBContext(DbContextOptions<ProsperityBankDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ ProsperityBank/Migrations/20220718114859_initialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProsperityBank.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TFN = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    State = table.Column<int>(type: "int", maxLength: 1, nullable: false),
+                    Postcode = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
