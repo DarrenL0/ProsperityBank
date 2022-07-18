533271c92036a8f2cae3a161ae2e60aed39bde8a
Darren <darren.lieu2000@gmail.com>
2022-07-18 21:19:28 +1000

    Add project files.

---
+++ ProsperityBank/Models/Customer.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+
+
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProsperityBank.Models
+{
+
+    public enum StateType
+    {
+        VIC = 1,
+        NSW = 2,
+        SA = 3,
+        QLD = 4,
+        TAS =5
+    }
+    public class Customer
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Customer ID")]
+        public int CustomerId { get; set; }
+
+        [Required, StringLength(50)]
+        [Display(Name = "Customer Name")]
+        public string CustomerName { get; set; }
+
+        [StringLength(11)]
+        public string TFN { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(1)]
+        public StateType State { get; set; }
+
+        [StringLength(4)]
+        public string Postcode { get; set; }
+
+        [Required, StringLength(15)]
+        [RegularExpression(@"^(61)-\d{​​​​9}​​​​$", ErrorMessage = "Must be of the format:(61)-XXXXXXXXX")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }   
+
+
+    }
+}
